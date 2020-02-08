cc36d23e8fe9d7a98421a3384272802f77a8186d
emreerkmen <emre.erkmen@hotmail.com>
2020-02-08 23:03:14 +0300

    Adapter done. Bridge done.

---
+++ StructuralPatterns/Bridge/2-Bridge.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.StructuralPatterns.Bridge
+{
+    // The Abstraction defines the interface for the "control" part of the two
+    // class hierarchies. It maintains a reference to an object of the
+    // Implementation hierarchy and delegates all of the real work to this
+    // object.
+    class Abstraction
+    {
+        protected IImplementation _implementation;
+
+        public Abstraction(IImplementation implementation)
+        {
+            this._implementation = implementation;
+        }
+
+        public virtual string Operation()
+        {
+            return "Abstract: Base operation with:\n" +
+                _implementation.OperationImplementation();
+        }
+    }
+
+    // You can extend the Abstraction without changing the Implementation
+    // classes.
+    class ExtendedAbstraction : Abstraction
+    {
+        public ExtendedAbstraction(IImplementation implementation) : base(implementation)
+        {
+        }
+
+        public override string Operation()
+        {
+            return "ExtendedAbstraction: Extended operation with:\n" +
+                base._implementation.OperationImplementation();
+        }
+    }
+
+    // The Implementation defines the interface for all implementation classes.
+    // It doesn't have to match the Abstraction's interface. In fact, the two
+    // interfaces can be entirely different. Typically the Implementation
+    // interface provides only primitive operations, while the Abstraction
+    // defines higher- level operations based on those primitives.
+    public interface IImplementation
+    {
+        string OperationImplementation();
+    }
+
+    // Each Concrete Implementation corresponds to a specific platform and
+    // implements the Implementation interface using that platform's API.
+    class ConcreteImplementationA : IImplementation
+    {
+        public string OperationImplementation()
+        {
+            return "ConcreteImplementationA: The result in platform A.\n";
+        }
+    }
+
+    class ConcreteImplementationB : IImplementation
+    {
+        public string OperationImplementation()
+        {
+            return "ConcreteImplementationA: The result in platform B.\n";
+        }
+    }
+
+    class Client
+    {
+        // Except for the initialization phase, where an Abstraction object gets
+        // linked with a specific Implementation object, the client code should
+        // only depend on the Abstraction class. This way the client code can
+        // support any abstraction-implementation combination.
+        public void ClientCode(Abstraction abstraction)
+        {
+            Console.Write(abstraction.Operation());
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+
+            Abstraction abstraction;
+            // The client code should be able to work with any pre-configured
+            // abstraction-implementation combination.
+            abstraction = new Abstraction(new ConcreteImplementationA());
+            client.ClientCode(abstraction);
+
+            Console.WriteLine();
+
+            abstraction = new ExtendedAbstraction(new ConcreteImplementationB());
+            client.ClientCode(abstraction);
+        }
+    }
+}
+
+/*
+    // The "abstraction" defines the interface for the "control"
+    // part of the two class hierarchies. It maintains a reference
+    // to an object of the "implementation" hierarchy and delegates
+    // all of the real work to this object.
+    class RemoteControl is
+        protected field device: Device
+        constructor RemoteControl(device: Device) is
+            this.device = device
+        method togglePower() is
+            if (device.isEnabled()) then
+                device.disable()
+            else
+                device.enable()
+        method volumeDown() is
+            device.setVolume(device.getVolume() - 10)
+        method volumeUp() is
+            device.setVolume(device.getVolume() + 10)
+        method channelDown() is
+            device.setChannel(device.getChannel() - 1)
+        method channelUp() is
+            device.setChannel(device.getChannel() + 1)
+
+
+    // You can extend classes from the abstraction hierarchy
+    // independently from device classes.
+    class AdvancedRemoteControl extends RemoteControl is
+        method mute() is
+            device.setVolume(0)
+
+
+    // The "implementation" interface declares methods common to all
+    // concrete implementation classes. It doesn't have to match the
+    // abstraction's interface. In fact, the two interfaces can be
+    // entirely different. Typically the implementation interface
+    // provides only primitive operations, while the abstraction
+    // defines higher-level operations based on those primitives.
+    interface Device is
+        method isEnabled()
+        method enable()
+        method disable()
+        method getVolume()
+        method setVolume(percent)
+        method getChannel()
+        method setChannel(channel)
+
+
+    // All devices follow the same interface.
+    class Tv implements Device is
+        // ...
+
+    class Radio implements Device is
+        // ...
+
+
+    // Somewhere in client code.
+    tv = new Tv()
+    remote = new RemoteControl(tv)
+    remote.togglePower()
+
+    radio = new Radio()
+    remote = new AdvancedRemoteControl(radio) 
+ */
